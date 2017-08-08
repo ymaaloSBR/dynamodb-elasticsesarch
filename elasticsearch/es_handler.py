6f6d5694f107b52dced57c7844e313dad62017b5
Yonathan Maalo <ymaalo@stansberryresearch.com>
2017-08-08 17:49:13 -0400

    Kinesis records partly handled

--- elasticsearch/es_handler.py
+++ elasticsearch/es_handler.py
@@ -3,7 +3,9 @@ from __future__ import print_function
 import json
 import base64
 import re
+import logging
 import os
+from sys import stdout
 from elasticsearch import Elasticsearch
 
 
@@ -13,11 +15,17 @@ from elasticsearch import Elasticsearch
 # Use IAM Role for authentication
 # Properly unmarshal DynamoDB JSON types. Binary NOT tested.
 
+FORMAT = '%(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-def decode_record_data(record):
-    dec = ''
-    base64.decode(record['data'], dec)
 
+def decode_kinesis_data(record):
+    logging.info("Decoding Kinesis Record...")
+    dec = base64.b64decode(record['kinesis']['data']).decode("utf-8")
+    logging.info("Kinesis Record Data Decoded:")
+    logging.info(dec)
     return dec
 
 
@@ -27,42 +35,42 @@ def process_stream(event, context):
         [os.environ['ES_ENDPOINT']],
     )
 
-    print("Cluster info:")
-    print(es.info())
+    logging.info("Cluster info:")
+    logging.info(es.info())
 
     # Loop over the DynamoDB Stream records
     for record in event['Records']:
 
-        print("New Record to process:")
-        print(json.dumps(record))
+        logging.info("New Record to process:")
+        logging.info(json.dumps(record))
         try:
 
             if record['eventName'] == "INSERT" or record['eventName'] == "aws:kinesis:record":
                 insert_document(es, record)
-            elif record['eventName'] == "REMOVE": #|| record['eventName'] == "aws:kinesis:record"#:
+            elif record['eventName'] == "REMOVE":
                 remove_document(es, record)
-            elif record['eventName'] == "MODIFY": #|| record['eventName'] == "aws:kinesis:record"#:
+            elif record['eventName'] == "MODIFY":
                 modify_document(es, record)
 
         except Exception as e:
-            print(e)
+            logging.exception(e)
             continue
 
 
 # Process MODIFY events
 def modify_document(es, record):
     table = get_table(record)
-    print("Dynamo Table: " + table)
+    logging.info("Dynamo Table: " + table)
 
     doc_id = generate_id(record)
-    print("KEY")
-    print(doc_id)
+    logging.info("KEY")
+    logging.info(doc_id)
 
     # Unmarshal the DynamoDB JSON to a normal JSON
     doc = json.dumps(unmarshal_json(record['dynamodb']['NewImage']))
 
-    print("Updated document:")
-    print(doc)
+    logging.info("Updated document:")
+    logging.info(doc)
 
     # We reindex the whole document as ES accepts partial docs
     es.index(index=table,
@@ -71,63 +79,64 @@ def modify_document(es, record):
              doc_type=table,
              refresh=True)
 
-    print("Success - Updated index ID: " + doc_id)
+    logging.info("Success - Updated index ID: " + doc_id)
 
 
 # Process REMOVE events
 def remove_document(es, record):
     table = get_table(record)
-    print("Dynamo Table: " + table)
+    logging.info("Dynamo Table: " + table)
 
     doc_id = generate_id(record)
-    print("Deleting document ID: " + doc_id)
+    logging.info("Deleting document ID: " + doc_id)
 
     es.delete(index=table,
               id=doc_id,
               doc_type=table,
               refresh=True)
 
-    print("Successly removed")
+    logging.info("Successly removed")
 
 
 # Process INSERT events
 def insert_document(es, record):
     table = get_table(record)
-    print("Dynamo Table: " + table)
+    logging.info("Dynamo Table: " + table)
 
     # Create index if missing
     if not es.indices.exists(table):
-        print("Create missing index: " + table)
+        logging.info("Create missing index: " + table)
 
         es.indices.create(table,
                           body='{"settings": { "index.mapping.coerce": true } }')
 
-        print("Index created: " + table)
+        logging.info("Index created: " + table)
 
     # Unmarshal the DynamoDB JSON to a normal JSON
     doc = ''
     if record['eventName'] == "aws:kinesis:record":
-        record_data = decode_record_data(record)
-        doc = json.dumps(unmarshal_json(record_data['dynamodb']['NewImage']))
+        record = json.loads(decode_kinesis_data(record))
+        doc = json.dumps(unmarshal_json(record['NewImage']))
     else:
         doc = json.dumps(unmarshal_json(record['dynamodb']['NewImage']))
 
-    print("New document to Index:")
-    print(doc)
+    logging.info("New document to Index:")
+    logging.info(doc)
 
     new_id = generate_id(record)
+    logging.info("Indexing into Elasticsearch...")
     es.index(index=table,
              body=doc,
              id=new_id,
              doc_type=table,
              refresh=True)
 
-    print("Success - New Index ID: " + new_id)
+    logging.info("Success - New Index ID: " + new_id)
 
 
 # Return the dynamoDB table that received the event. Lower case it
 def get_table(record):
-    p = re.compile('arn:aws:(?:dynamodb|kinesis):.*?:.*?:table/([0-9a-zA-Z_-]+)/.+')
+    p = re.compile('arn:aws:(?:dynamodb|kinesis):.*?:(?:table|stream)/([\w-]+)(?:.)*')
     m = p.match(record['eventSourceARN'])
     if m is None:
         raise Exception("Table not found in SourceARN")
@@ -136,8 +145,12 @@ def get_table(record):
 
 # Generate the ID for ES. Used for deleting or updating item later
 def generate_id(record):
-    keys = unmarshal_json(record['dynamodb']['Keys'])
-
+    logging.info("Generating ID")
+    if 'dynamodb' in record:
+        keys = unmarshal_json(record['dynamodb']['Keys'])
+    else:
+        keys = unmarshal_json(record['Keys'])
+    logging.info("Keys in record: " + json.dumps(keys))
     # Concat HASH and RANGE key with | in between
     new_id = ""
     i = 0
@@ -152,6 +165,7 @@ def generate_id(record):
 
 # Unmarshal a JSON that is DynamoDB formatted
 def unmarshal_json(node):
+    logging.info("Unmarshalling record...")
     data = {"M": node}
     return unmarshal_value(data, True)
 
